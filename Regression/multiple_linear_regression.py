33895950267025e8b291ddca872686ef5bf48be5
fkhjoy <khjoyfoysal@gmail.com>
2019-03-22 21:49:55 +0600

    added polynomial regression and position salaried data

--- Regression/multiple_linear_regression.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 22 00:41:05 2019
-
-@author: user
-"""
-
---
+++ Regression/polynomial_regression.py
@@ -0,0 +1,42 @@
+#importing libraries
+import pylab as plt
+import pandas as pd
+
+#importing dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+#fitting linear regression
+from sklearn.linear_model import LinearRegression
+lin_regressor = LinearRegression()
+lin_regressor.fit(X, y)
+
+#fitting polynomial regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_regressor = PolynomialFeatures(degree = 2)
+X_poly = poly_regressor.fit_transform(X)
+lin_regressor2 = LinearRegression()
+lin_regressor2.fit(X_poly, y)
+
+#visualization of linear regression resutls
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_regressor.predict(X))
+plt.title('Linear Regression (Truth)')
+plt.xlabel('Positoin Label')
+plt.ylabel('Salary')
+plt.show()
+
+#visualization of polynomial linear regression
+plt.scatter(X, y, color =  'red')
+plt.plot(X, lin_regressor2.predict(poly_regressor.fit_transform(X)))
+plt.title('Polynomial Regression')
+plt.xlabel('Position Label')
+plt.ylabel('Salary')
+plt.show()
+
+#predicting with linear regression
+lin_regressor.predict(6.5)
+
+#predicting with polynomial regression
+lin_regressor2.predict(poly_regressor.fit_transform(6.5))
